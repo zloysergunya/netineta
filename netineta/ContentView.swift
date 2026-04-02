898bbee3a3bc48ecd703bcc56744d4fcbbcb0e1a
zloysergunya <zloysergunya@mail.ru>
2026-04-02 22:44:29 +0300

    Initial Commit

---
+++ netineta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  netineta
+//
+//  Created by Sergey Kotov on 02.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ netineta/netinetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  netinetaApp.swift
+//  netineta
+//
+//  Created by Sergey Kotov on 02.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct netinetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
